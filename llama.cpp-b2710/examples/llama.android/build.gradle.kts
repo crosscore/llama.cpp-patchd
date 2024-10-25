7f7009a9083dfeca3f1754d0477a3be16b2c71f9
yuu <crosscore.g@gmail.com>
2024-10-25 14:58:12 +0900

    delete 'build*' from .gitignore

---
+++ llama.cpp-b2710/examples/llama.android/build.gradle.kts
@@ -0,0 +1,7 @@
+// llama.android/build.gradle.kts
+
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    id("com.android.application") version "8.6.1" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+}
